b0e2f009826beeeeb1e5b0b4f9ba8569a1201c29
Tom Marmoz <tom.marmoz@epitech.eu>
2026-01-10 15:24:58 +0100

    [+] - Added version check & installation script basic. The installation scripts now firstly check for the current installation.

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+readonly BASEDIR=~/.3pf
+
+
+
+if [ -f $BASEDIR/config ]; then
+
+    echo "3PF is already installed on this computer, verifying installation..."
+    python3 ./tools/check_3pf_install.py $BASEDIR/config
+    readonly VERSION_CHECKING_SIGNAL=$(echo $?)
+
+    if [ $VERSION_CHECKING_SIGNAL -eq 0 ]; then
+        exit 0
+    elif [ $VERSION_CHECKING_SIGNAL -eq 2 ]; then
+        echo "Launching Full Reinstall.."
+        rm -rf $BASEDIR
+    elif [ $VERSION_CHECKING_SIGNAL -eq 3 ]; then
+        echo "Launching Reinstall..."
+        rm -f $BASEDIR/config
+    else
+        exit 0
+    fi
+fi
+
+
+echo "Installing 3PF..."
+
+
+# ===== Init Base subfolders ===== #
+
+mkdir -p $BASEDIR
+mkdir -p $BASEDIR/libs/
+
+touch $BASEDIR/config
+echo $(cat const) > $BASEDIR/config
+
