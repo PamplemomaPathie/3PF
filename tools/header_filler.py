993a7acaa546dc331e5c998d250467605ff5b774
TomH <tom.heirich@epitech.eu>
2026-01-12 16:03:02 +0100

    [+] - Basic parsing of function prototypes from .c files. The header filler can parse .c source files and retrieve function prototypes.

--- tools/header_filler.py
+++ tools/header_filler.py
@@ -12,7 +12,13 @@ def read_file(filename: str):
     return content
 
 def get_function_prototypes(content: str):
-    lala = content.split("\n")
+    lala = content.split("}\n\n")
+    for i in range(len(lala)):
+        tmp = lala[i].split("{", 1)
+        if len(tmp) > 0:
+            lala[i] = tmp[0].strip("\n")
+    tmp = lala[0].split("\n")
+    lala[0] = tmp[len(tmp) - 1]
     return lala
 
 def main():
@@ -20,6 +26,8 @@ def main():
     print(file)
     content = read_file(file)
     funcprototypes = get_function_prototypes(content)
-    print(funcprototypes)
+    for func in funcprototypes:
+        print(func)
+        print("=====")
 
 main()
