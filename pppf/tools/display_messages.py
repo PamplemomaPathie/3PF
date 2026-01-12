a81ba1f62985c4f76fe9be70af10ff7bc62860d6
Tom Marmoz <tom.marmoz@epitech.eu>
2026-01-12 14:24:08 +0100

    [~] - Changed the help display message. Self Explanatory.

--- pppf/tools/display_messages.py
+++ pppf/tools/display_messages.py
@@ -22,3 +22,4 @@ Available Commands:
         help_text += f"  {arg_name}{' ' * (7 - len(arg_name))}\t{arg_info.get('description', '')}\n"
 
     print(help_text)
+    print('Use "3pf [command] help" for more information about a command.')
