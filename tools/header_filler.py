483d93af2f5a0446ee17ad905709885214a67ac6
TomH <tom.heirich@epitech.eu>
2026-01-13 12:30:38 +0100

    [+] - Started header parsing. The header filler function needs to parse a given header to insert the function prototypes it retrieved in the right location.

--- tools/header_filler.py
+++ tools/header_filler.py
@@ -31,13 +31,20 @@ def get_function_prototypes(content: str):
     result = [res for res in result if res[-1:] == ';'] # filtering prototypes to remove those without semicolon (since it means they're a static function)
     return result
 
+def insert_prototypes_in_header(prototypes, header):
+    contents = read_file(header)
+    result = contents.splitlines()
+    for line in result:
+        print(line)
+
 def main():
     file = sys.argv[1]
-    print(file)
-    content = read_file(file)
-    funcprototypes = get_function_prototypes(content)
-    for func in funcprototypes:
+    header = sys.argv[2]
+    prototypes = get_function_prototypes(read_file(file))
+    for func in prototypes:
         print(func)
-        print("=====")
+        print("====")
+    insert_prototypes_in_header(prototypes, header)
 
-main()
+if __name__ == "__main__":
+    main()
