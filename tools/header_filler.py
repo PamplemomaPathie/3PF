92f4275cca53077c93cb80af3595125e337ee874
TomH <tom.heirich@epitech.eu>
2026-01-16 13:50:18 +0100

    [-] - Removed header parsing. The header filler will instead create the header of the imported library.

--- tools/header_filler.py
+++ tools/header_filler.py
@@ -31,20 +31,12 @@ def get_function_prototypes(content: str):
     result = [res for res in result if res[-1:] == ';'] # filtering prototypes to remove those without semicolon (since it means they're a static function)
     return result
 
-def insert_prototypes_in_header(prototypes, header):
-    contents = read_file(header)
-    result = contents.splitlines()
-    for line in result:
-        print(line)
-
 def main():
     file = sys.argv[1]
-    header = sys.argv[2]
     prototypes = get_function_prototypes(read_file(file))
     for func in prototypes:
         print(func)
         print("====")
-    insert_prototypes_in_header(prototypes, header)
 
 if __name__ == "__main__":
     main()
