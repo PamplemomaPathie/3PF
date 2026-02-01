811d0feeb0e99e10b8eea4c748a68bec46379915
PamplemomM <tom.marmoz@epitech.eu>
2026-02-01 19:51:46 +0100

    [+] - Enhanced 'reload' command for better custom prints.

--- pppf/reload_packets.py
+++ pppf/reload_packets.py
@@ -9,8 +9,9 @@ import os
 import sys
 
 
-def warning_print(content: str):
-    print(f"\033[1;35mWarning\033[0m: {content}")
+def warning_print(content: str, enable_printing: bool):
+    if enable_printing:
+        print(f"\033[1;35mWarning\033[0m: {content}")
 
 """
 Return the inside content of a libraries directory.
@@ -55,40 +56,38 @@ def get_lib_headers(path, path_content):
 
 
 def get_all_prototypes(lib_dir):
-    print(lib_dir)
     return read_file(lib_dir + "content.txt")
 
-def get_lib_info(lib, content):
+def get_lib_info(lib, content, warnings_printing: bool):
+    wp = warnings_printing
     default_lib = {
         "content": None,
         "desc": None,
         "versions": {}
     }
     lib_dir = LIBDIR + lib + "/"
-    print(lib, content)
-    print(lib_dir)
     if "content.txt" in content:
         default_lib["content"] = get_all_prototypes(lib_dir)
     else:
-        warning_print(f"Missing 'content.txt' file in '{lib}' library.")
+        warning_print(f"\033[1m{lib}\033[0m: Missing '\033[1mcontent.txt\033[0m' file.", wp)
     if "desc.txt" in content:
         default_lib["desc"] = read_file(lib_dir + "desc.txt")
     else:
-        warning_print(f"Missing 'desc.txt' file in '{lib}' library.")
+        warning_print(f"\033[1m{lib}\033[0m: Missing '\033[1mdesc.txt\033[0m' file.", wp)
     if "details.json" in content:
         content = load_from_json(lib_dir + "details.json")
         default_lib["links"] = content
     else:
-        warning_print(f"Missing 'details.json' file in '{lib}' library.")
+        warning_print(f"\033[1m{lib}\033[0m: Missing '\033[1mdetails.json\033[0m' file.", wp)
     return default_lib
 
 
-def reload_libs():
+def reload_libs(warnings: bool = False):
     lib_list = get_libs_inside_content(LIBDIR)
 
     libs = {}
     for lib in lib_list:
-        current_lib = get_lib_info(lib, lib_list[lib])
+        current_lib = get_lib_info(lib, lib_list[lib], warnings)
         lib_dir = LIBDIR + lib + "/"
         for version in lib_list[lib]:
             current_path = lib_dir + version
@@ -100,7 +99,6 @@ def reload_libs():
             current_lib["versions"][version]["tests"] = get_lib_tests(current_path + "/", content)[0]
             current_lib["versions"][version]["headers"] = get_lib_headers(current_path + "/", content)
         libs[lib] = current_lib
-    print(libs)
     save_to_json(libs, BASEDIR + "libs.json")
 
 
@@ -111,4 +109,5 @@ def reload_command(args):
 
     reload.parse(args)
 
-    reload_libs()
+    reload_libs(warnings=True)
+    print(f"\033[1;32mReloaded your libraries successfully\033[0m!")
