90ada0de33284e43b11d694e64e9f60e94233fa1
Tom Marmoz <tom.marmoz@epitech.eu>
2026-02-02 10:59:26 +0100

    [~] - Adapted 'install' command to the ArgumentArsenal class.

--- pppf/install_packet.py
+++ pppf/install_packet.py
@@ -5,18 +5,6 @@ import sys
 
 
 
-def print_usage():
-    print("Usage: 3pf install <packet-name> <dest> [flags]\n")
-    print("\nInstall a <packet-name> packet to a <dest> directory.")
-    print("\nFlags:")
-    print("  --help\t\tHelp for install command.")
-    print("  --tests\t\tInstall packet with its unit test files.")
-    print("  --version <version>\tInstall a specific version of a packet.")
-    print("  --no-dependencies\tInstall packet without its dependencies.")
-    print("\nExample:")
-    print('  3pf install "myOwnLib"\n  --version 2')
-
-
 def flag_tests(lib, args, i) -> bool:
     lib["unit-tests"] = True
     return True
@@ -35,61 +23,34 @@ def flag_nodeps(lib, args, i) -> bool:
     return True
 
 
-flags = {
-    "--tests": {
-        "required": 0,
-        "function": flag_tests
-    },
-    "--version": {
-        "required": 1,
-        "function": flag_version
-    },
-    "--no-dependencies": {
-        "required": 0,
-        "function": flag_nodeps
-    }
-}
-
-def parse_arguments(args, lib):
-    current_param = 0
-
-    for i in range(len(args)):
-        if current_param > 0:
-            current_param -= 1
-            continue
-
-        if args[i] in flags:
-            current = flags[args[i]]
-
-            if current.get("required", 0) + i + 1 > len(args):
-                print(f"Error: '{args[i][2:]}' flag requires {current.get('required', 0)} parameters.")
-                sys.exit(1)
-
-            func = current.get("function", None)
-            if func != None:
-                if func(lib, args, i) == False:
-                    sys.exit(2)
-                current_param = current.get("required", 0)
-
-        elif args[i].startswith("--"):
-            print(f"Error: '{args[i]}' unknown flag in 'deploy' command.")
-            sys.exit(1)
-
 
 def install_packet(args):
 
-    if len(args) <= 1 or args[0] == "help" or args[0] == "--help":
-        print_usage()
-        sys.exit(0)
-
-    lib = {
+    options = {
         "unit-tests": False,
         "version": 0, # 0 for latest version
         "dependencies": True
     }
 
-    parse_arguments(args, lib)
-    if lib["version"] == 0:
-        lib["version"] = lib["version"] # REMINDER TO SET TO LAST VERSION
-    print(lib)
+    deploy_command = ArgumentArsenal("install", options,
+        args=["packet-name", "dest"],
+        desc="Install a <packet-name> packet to a <dest> directory.",
+        additional="")
+
+    deploy_command.make_flag("--test", [], flag_tests,
+        "Install packet with its unit test files.")
+    deploy_command.make_flag("--version", ["version"], flag_version,
+        "Install a specific version of a packet.")
+    deploy_command.make_flag("--no-dependencies", [], flag_nodeps,
+        "Install packet without its dependencies.")
+
+    deploy_command.parse(args)
+
+    user_args = deploy_command.get_args()
+    name = user_args[0]
+    dest = user_args[1]
+
+    if options["version"] == 0:
+        options["version"] = options["version"] # REMINDER TO SET TO LAST VERSION
+    print(options)
 
