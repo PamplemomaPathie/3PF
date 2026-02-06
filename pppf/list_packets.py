03df26b29456f0053b2452df34912f6f6665f52a
PamplemomM <tom.marmoz@epitech.eu>
2026-02-06 22:29:29 +0100

    [~] - Reworked all 'list' command to be more easy to use.

--- pppf/list_packets.py
+++ pppf/list_packets.py
@@ -3,7 +3,7 @@
 from arsenals.argument_arsenal import ArgumentArsenal
 from const import BASEDIR
 from tools.json_tools import load_from_json
-from pppf.pppf_tools import load_libs
+from pppf.pppf_tools import load_libs, get_all_groups
 import sys
 
 
@@ -46,15 +46,40 @@ def print_lib(name: str, lib, details: bool = True):
 
 def display_libs(options):
     libs = load_libs()
-    custom_libs = options.get("lib", [])
-    print(f"3PF Available libs: {len(libs)}")
-    if custom_libs != []:
-        print(f"Selected {len(custom_libs)} librar{'y' if len(custom_libs) == 1 else 'ies'}: {', '.join(custom_libs)}")
-    print('')
+    custom_libs = options["lib"]
+
+    groups = {}
     for lib in libs:
-        if not (lib in custom_libs or custom_libs == []):
-            continue
-        print_lib(lib, libs[lib], details=options["detail"])
+        group = libs[lib].get("group", None)
+        if group != None:
+            if groups.get(group, None) == None:
+                groups[group] = []
+            groups[group].append(lib)
+
+    if len(custom_libs) == 0:
+        print(f"3PF Available groups: {len(groups)}")
+        print(f"    Available libs: {len(libs)}")
+        for group in groups:
+            print("")
+            print(f"- \033[1m{group}\033[0m - \033[1m{len(groups[group])}\033[0m packets.")
+            for content in groups[group]:
+                print(f"    \033[3m{content}\033[0m")
+        print("\n\033[3mPlease give us a group/library name to get info about it.\033[0m")
+    elif custom_libs[0] in groups:
+        print(f"Selected \033[1m{custom_libs[0]}\033[0m group.")
+        printed = 0
+        for lib in libs:
+            if libs[lib].get("group", None) == custom_libs[0]:
+                if options["detail"] == True or printed == 0:
+                    print("")
+                printed += 1
+                print_lib(lib, libs[lib], details=options["detail"])
+    else:
+        print(f"Selected {len(custom_libs)} librar{'y' if len(custom_libs) == 1 else 'ies'}: {', '.join(custom_libs)}")
+        for lib in libs:
+            if lib in custom_libs or custom_libs == []:
+                print("")
+                print_lib(lib, libs[lib], details=options["detail"])
 
 
 def flagD(args, options) -> bool:
@@ -73,15 +98,16 @@ def list_packets(args):
     }
 
     list_command = ArgumentArsenal("list", options, args=[],
-      desc="List all installed packets.", additional=
+      desc="List all installed packets and groups.", additional=
       "You can also display more info about specific packets by naming them.")
 
-    list_command.enable_va_arg("packets", handleVaArgArgument)
+    list_command.enable_va_arg("group/packets", handleVaArgArgument)
     list_command.make_flag("--simple", [], flagD, "Don't display packet details.")
 
     list_command.parse(args)
+    display_libs(options)
     try:
-        display_libs(options)
+        print("", end='')
     except Exception as e:
         print("Wrong configuration of 3PF, please consider reinstalling the client.")
         print(f"Error detail: {e}")
